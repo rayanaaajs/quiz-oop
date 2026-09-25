7499a0d4aa327bd38aa9869d1c76ee2139501932
rayanaaajs <rayanajakasurya54@gmail.com>
2026-09-25 09:16:09 +0700

    set name test

--- src/main/java/id/ac/polinema/oop/Main.java
+++ src/main/java/id/ac/polinema/oop/Main.java
@@ -17,6 +17,7 @@ public class Main {
     menu.addMenuItem(new MenuItem("Roti Bakar", 12000));
 
     Customer budi = new Customer("C001", "Budi Santoso");
+    budi.setName("Budi S");
     Order order = new Order(budi);
     order.addItem(menu.findItem("Es Kopi Susu"), 2);
     order.addItem(menu.findItem("Roti Bakar"), 1);
